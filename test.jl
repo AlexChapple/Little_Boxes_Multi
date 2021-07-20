36a5e4ff003d7d6652a345537bcf064fd3904c0c
Alex <alex.a.chapple@gmail.com>
2021-07-20 15:35:37 +1200

    update

--- test.jl
+++ test.jl
@@ -5,29 +5,43 @@ Performance tester
 
 function adder1()
 
-    total::Float64 = 10000
-    total_list::Array{Float64} = zeros(1,800000)
+    z::Array{ComplexF64} = zeros(1,10^7)
+    z_mod::Float64 = 0
 
-    for i in 1:800000
-        total /= i
-        total_list[i] = total 
+    for i in 1:10^7
+
+        z[i] = rand() + im*rand()
+
     end
 
-    return total, total_list
+    for i in z
+
+        z_mod += sqrt(conj(i) * i)
+
+    end
+
+    print(z_mod)
 
 end
 
 function adder2()
 
-    total::Float64 = 10000
-    total_list::Array{Float64}  = zeros(1,800000)
+    z::Array{ComplexF64} = zeros(1,10^7)
+    z_mod::Float64 = 0
 
-    for i in 1:800000
-        total = total / i
-        total_list[i] = total 
+    for i in 1:10^7
+
+        z[i] = rand() + im*rand()
+
     end
 
-    return total, total_list
+    for i in z
+
+        z_mod += sqrt(real(i)^2 + imag(i)^2)
+
+    end
+
+    print(z_mod)
 
 end
 
